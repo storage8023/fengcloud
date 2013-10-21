bad1faaef46f1e4e678ccf55b8f077c334d81669
GeorgeXu <xg@gokuai.com>
2013-10-21 15:14:51 +0800

    去掉GKsession

--- app/scripts/interface.js
+++ app/scripts/interface.js
@@ -81,7 +81,7 @@ var gkClientInterface = {
         }
     },
     getUser:function(){
-        return JSON.parse(gkClient.gUserinfo());
+        return JSON.parse(gkClient.gUserInfo());
     },
     saveToLocal:function(params){
         try {
