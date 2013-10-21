03dde048e5fca861436d70a0435385b397c60d08
GeorgeXu <xg@gokuai.com>
2013-10-21 14:19:56 +0800

    去掉测试代码

--- app/scripts/interface.js
+++ app/scripts/interface.js
@@ -20,266 +20,6 @@ var gkClientInterface = {
      */
     getFileList: function (params) {
         try {
-            var list = [];
-            if (params.webpath == 'aaa') {
-                list = [
-                    {
-                        "path": "aaa/测试1",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 12345678,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "aaa/测试3",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 12345678,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "aaa/测试1",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 12345678,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "aaa/测试3",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 12345678,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "aaa/测试1",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 12345678,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "aaa/测试3",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 12345678,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "aaa/测试1",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 12345678,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "aaa/测试3",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 12345678,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    }
-                ]
-            } else {
-                    list =  [
-                    {
-                        "path": "aaa",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 1381568404,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 0,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "bbb",
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 0,
-                        "uuidhash": "",
-                        "lasttime": 1381568504,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 0,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "ccc.jpg",
-                        "dir": 0,
-                        "filehash": "",
-                        "filesize": 123456,
-                        "uuidhash": "",
-                        "lasttime": 1381568604,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 2,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    },
-                    {
-                        "path": "ddd.doc",
-                        "dir": 0,
-                        "filehash": "",
-                        "filesize": 12345,
-                        "uuidhash": "",
-                        "lasttime": 1381568604,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 0,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    }
-
-                ];
-                if(params.debug){
-                    list.push({
-                        "path": params.debug,
-                        "dir": 1,
-                        "filehash": "",
-                        "filesize": 2121212,
-                        "uuidhash": "",
-                        "lasttime": 1381568604,
-                        "lastid": 111,
-                        "lastname": "aaa",
-                        "creatortime": 12345678,
-                        "creatorid": 111,
-                        "creatorname": "111",
-                        "lock": 1,
-                        "locktime": 12345678,
-                        "lockid": 111,
-                        "lockname": "aaa",
-                        "status": 4,
-                        "version": 123
-                    })
-                }
-
-            }
-            return list;
             return gkClient.gGetFileList(JSON.stringify(params));
         } catch (e) {
             this._handleException(e);
@@ -296,16 +36,6 @@ var gkClientInterface = {
      */
     getSideTreeList: function (params) {
         try {
-            return [
-                {
-                    name: "够快科技",
-                    mountid: 2
-                },
-                {
-                    name: "上海够快",
-                    mountid: 5
-                }
-            ];
             return gkClient.gSideTreeList(JSON.stringify(params));
         } catch (e) {
             this._handleException(e);
@@ -317,12 +47,6 @@ var gkClientInterface = {
      */
     addFileDialog: function () {
         try {
-            return {
-                'list': [
-                    {'path': 'aaa'},
-                    {'path': 'bbb'}
-                ]
-            };
             return JSON.parse(gkClient.gAddFileDlg());
         } catch (e) {
             this._handleException(e);
@@ -330,7 +54,6 @@ var gkClientInterface = {
     },
     addFile: function (params) {
         try {
-            return {error:0,message:'test'};
             return JSON.parse(gkClient.gAdd(JSON.stringify(params)));
         } catch (e) {
             this._handleException(e);
@@ -345,7 +68,6 @@ var gkClientInterface = {
     },
     lock: function (params) {
         try {
-            return {error:0,message:'test'};
             return JSON.parse(gkClient.lock(JSON.stringify(params)));
         } catch (e) {
             this._handleException(e);
@@ -353,32 +75,16 @@ var gkClientInterface = {
     },
     unlock: function (params) {
         try {
-            return {error:0,message:'test'};
             return JSON.parse(gkClient.unlock(JSON.stringify(params)));
         } catch (e) {
             this._handleException(e);
         }
     },
     getUser:function(){
-        return {
-            "org_id":1,
-            "id":2,
-            "email":"xugetest1@126.com",
-            "username":"测试1",
-            "org_username":"123",
-            "photourl":"http://oss.aliyuncs.com/gkavatar2/39/398fd1f3fb5f3f7b1077d623c5ade70b1c63b50b.jpg",
-            "mount_id":2,
-            "capacity":0,
-            "size":59321948,
-            "org_name":"web开发组",
-            "org_size":108209585
-        }
-
         return JSON.parse(gkClient.gUserinfo());
     },
     saveToLocal:function(params){
         try {
-            return {error:0,message:'test'};
             return gkClient.gSaveToLocal(params);
         } catch (e) {
             this._handleException(e);
@@ -386,7 +92,6 @@ var gkClientInterface = {
     },
     del:function(params){
         try {
-            return {error:0,message:'test'};
             return gkClient.gDel(params);
         } catch (e) {
             this._handleException(e);
@@ -394,7 +99,6 @@ var gkClientInterface = {
     },
     rename:function(params){
         try {
-            return {error:0,message:'test'};
             return gkClient.gRename(params);
         } catch (e) {
             this._handleException(e);
@@ -402,7 +106,6 @@ var gkClientInterface = {
     },
     copy:function(params){
         try {
-            return {error:0,message:'test'};
             return gkClient.gCopy(params);
         } catch (e) {
             this._handleException(e);
@@ -410,7 +113,6 @@ var gkClientInterface = {
     },
     move:function(params){
         try {
-            return {error:0,message:'test'};
             return gkClient.gMove(params);
         } catch (e) {
             this._handleException(e);
@@ -426,7 +128,6 @@ var gkClientInterface = {
     },
     openLocation:function(params){
         try {
-            return {error:0,message:'test'};
             params.opentype = 'select';
             return gkClient.gOpen(params);
         } catch (e) {
@@ -435,7 +136,6 @@ var gkClientInterface = {
     },
     selectPath:function(){
         try {
-            return 'C://DDDD';
             return gkClient.gSelectPathDlg();
         } catch (e) {
             this._handleException(e);
@@ -464,7 +164,6 @@ var gkClientInterface = {
     },
     getRestHost:function(){
         try {
-            return 'http://r.gokuai.com';
             return gkClient.gRestHost();
         } catch (e) {
             this._handleException(e);
@@ -472,7 +171,6 @@ var gkClientInterface = {
     },
     getApiHost:function(){
         try {
-            return 'http://a.gokuai.com';
             return gkClient.gApiHost();
         } catch (e) {
             this._handleException(e);
