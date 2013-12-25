e2a62b354cf76227921256e0ade0d08e42294f18
GeorgeXu <xg@gokuai.com>
2013-12-25 10:48:05 +0800

    修复https切换后无法打开也能的bug

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,9 +3,17 @@ angular.module('LocalStorageModule').value('prefix', 'gkClientIndex');
 angular.module('gkClientIndex', ['jmdobry.angular-cache','pasvaz.bindonce','gkClientIndex.controllers', 'gkClientIndex.filters', 'gkClientIndex.directives', 'gkClientIndex.services', 'LocalStorageModule', 'ui.directives', 'ui.utils', 'ui.bootstrap', 'ngSanitize', 'LocalStorageModule'])
     .config(['$sceDelegateProvider',function ($sceDelegateProvider) {
         var siteDomain = gkClientInterface.getSiteDomain();
+        console.log(siteDomain);
         $sceDelegateProvider.resourceUrlWhitelist([
             'self',
-            siteDomain+'/**'
+            'http://*.goukuai.cn/**',
+            'https://*.goukuai.cn/**',
+            'http://*.gokuai.cn/**',
+            'https://*.gokuai.cn/**',
+            'http://*.gokuai.com/**',
+            'https://*.gokuai.com/**',
+            'http://*.yunku.cn/**',
+            'https://*.yunku.cn/**'
         ]);
     }])
 ;
