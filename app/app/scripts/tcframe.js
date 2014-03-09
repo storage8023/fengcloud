2d17dc8af4b102ba4b7de5a431d7b02e6d568610
GeorgeXu <xg@gokuai.com>
2014-03-09 10:40:50 +0800

    。

--- app/app/scripts/tcframe.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-angular.module('LocalStorageModule').value('prefix', 'gkClientIndex');
-angular.module('gkClientFrame',['GKCommon','gkClientIndex.filters','gkClientIndex.directives','gkClientIndex.services','gkClientFrame.controllers','LocalStorageModule','ngSanitize']);
